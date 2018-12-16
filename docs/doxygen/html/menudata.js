91129420d388c6babe3bc427698bf293a55f74fb
grady <mgradysaunders@gmail.com>
2018-12-16 16:37:38 -0500

    rerun doxygen

--- docs/doxygen/html/menudata.js
+++ docs/doxygen/html/menudata.js
@@ -31,7 +31,8 @@ var menudata={children:[
 {text:"o",url:"namespacemembers_func.html#index_o"},
 {text:"r",url:"namespacemembers_func.html#index_r"}]},
 {text:"Typedefs",url:"namespacemembers_type.html"},
-{text:"Enumerations",url:"namespacemembers_enum.html"}]}]},
+{text:"Enumerations",url:"namespacemembers_enum.html"},
+{text:"Enumerator",url:"namespacemembers_eval.html"}]}]},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
