e7c4b5c8a69cb44cfd383676a4c4f768bfde5eb4
grady <mgradysaunders@gmail.com>
2019-01-07 16:03:59 -0500

    more aabb

--- include/prefabs/aabb.hpp
+++ include/prefabs/aabb.hpp
@@ -66,6 +66,11 @@ class aabb
 {
 public:
 
+    // Sanity check.
+    static_assert(
+        std::is_arithmetic<T>::value,
+        "T must be arithmetic");
+
     /**
      * @brief Value type.
      */
@@ -75,6 +80,12 @@ public:
      * @brief Entry type.
      */
     typedef T entry_type;
+
+    /**
+     * @brief Float type.
+     */
+    typedef std::conditional_t<
+            std::is_floating_point<T>::value, T, double> float_type;
 
 public:
 
@@ -134,6 +145,11 @@ public:
 public:
 
     /**
+     * @name Misc
+     */
+    /**@{*/
+
+    /**
      * @brief Diagonal.
      */
     constexpr multi<T, N> diag() const
@@ -143,6 +159,16 @@ public:
 
     /**
      * @brief Hyper volume.
+     *
+     * @note
+     * Hyper volume in @f$ N @f$ dimensions measures the entire
+     * @f$ N @f$ dimensional subspace. That is,
+     * @f[
+     *      V = \prod_{k \in L} (P_{\max[k]} - P_{\min[k]})
+     * @f]
+     * for index set @f$ L = [0, N) @f$.
+     * - In @f$ N = 2 @f$ dimensions, @f$ V @f$ measures signed area.
+     * - In @f$ N = 3 @f$ dimensions, @f$ V @f$ measures signed volume.
      */
     constexpr T volume() const
     {
@@ -152,29 +178,53 @@ public:
 
     /**
      * @brief Hyper surface area.
+     *
+     * @note
+     * Hyper surface area in @f$ N @f$ dimensions measures the 
+     * @f$ N - 1 @f$ dimensional subspace on the border of the @f$ N @f$
+     * dimensional volume. That is,
+     * @f[
+     *      S = \sum_{k \in L} \prod_{j \in L_k} 2 (P_{\max[j]} - P_{\min[j]})
+     * @f]
+     * for index sets @f$ L = [0, N) @f$ and @f$ L_k = L \setminus k @f$. 
+     * - In @f$ N = 2 @f$ dimensions, @f$ S @f$ measures signed perimeter.
+     * - In @f$ N = 3 @f$ dimensions, @f$ S @f$ measures signed surface area.
      */
     constexpr T surface_area() const
     {
-        T a = T(0);
-        multi<T, N> d = diag();
-        for (std::size_t k = 0; k < N; k++) {
-            T t = T(1);
-            for (std::size_t j = 0; j < N; j++) {
-                if (j != k) {
-                    t *= d[j];
-                }
-            }
-            a += t;
+        if constexpr (N == 1) {
+            return 1;
         }
-        return T(2) * a;
+        else {
+            T a = T(0);
+            multi<T, N> d = diag();
+            for (std::size_t k = 0; k < N; k++) {
+                T t = T(1);
+                for (std::size_t j = 0; j < N; j++) {
+                    if (j != k) {
+                        t *= d[j];
+                    }
+                }
+                a += t;
+            }
+            return T(2) * a; // Wrong for N = 1.
+        }
     }
 
     /**
      * @brief Center.
      */
-    constexpr multi<T, N> center() const
+    constexpr multi<float_type, N> center() const
     {
-        return (arr_[0] + arr_[1]) / T(2);
+        return float_type(0.5) * (arr_[0] + arr_[1]);
+    }
+
+    /**
+     * @brief Radius.
+     */
+    float_type radius() const
+    {
+        return float_type(0.5) * pr::length(arr_[1] - arr_[0]);
     }
 
     /**
@@ -186,12 +236,22 @@ public:
         return (1 - u) * arr_[0] + u * arr_[1];
     }
 
+    /**@}*/
+
 public:
 
     /**
      * @name Tests
      */
     /**@{*/
+
+    /**
+     * @brief Hyper volume is non-negative?
+     */
+    constexpr operator bool() const
+    {
+        return (arr_[0] <= arr_[1]).all();
+    }
 
     /**
      * @brief Overlaps other?
@@ -280,11 +340,126 @@ public:
     }
 
     /**
-     * @brief Hyper volume is non-negative?
+     * @brief Ray information.
      */
-    constexpr operator bool() const
+    struct ray_info
     {
-        return (arr_[0] <= arr_[1]).all();
+        /**
+         * @brief Default constructor.
+         */
+        ray_info() = default;
+
+        /**
+         * @brief Constructor.
+         *
+         * @param[in] o
+         * Origin @f$ \mathbf{o} @f$.
+         *
+         * @param[in] d
+         * Direction @f$ \mathbf{d} @f$.
+         *
+         * @param[in] tmin
+         * Minimum parameter @f$ t_{\min} @f$.
+         *
+         * @param[in] tmax
+         * Maximum parameter @f$ t_{\max} @f$.
+         */
+        ray_info(
+                const multi<float_type, N>& o, 
+                const multi<float_type, N>& d,
+                float_type tmin = 0,
+                float_type tmax = pr::numeric_limits<float_type>::infinity()) :
+            o(o),
+            dinv(1 / d),
+            dmin(pr::signbit(d)),
+            dmax(1 - dmin),
+            tmin(tmin),
+            tmax(tmax)
+        {
+        }
+
+    public:
+
+        /**
+         * @brief Origin @f$ \mathbf{o} @f$.
+         */
+        multi<float_type, N> o;
+
+        /**
+         * @brief Direction inverse @f$ \mathbf{d}^{-1} @f$.
+         *
+         * @note
+         * @f[
+         *      d^{-1}_{[k]} = 1 / d_{[k]}
+         * @f]
+         */
+        multi<float_type, N> dinv;
+
+        /**
+         * @brief Direction minimum index @f$ \mathbf{d}_{\min} @f$.
+         *
+         * @note
+         * @f[
+         *      d_{\min[k]} =
+         *      \begin{cases}
+         *          1 & d_{[k]} < 0 
+         *      \\  0 & d_{[k]} \ge 0 
+         *      \end{cases}
+         * @f]
+         */
+        multi<int, N> dmin;
+
+        /**
+         * @brief Direction maximum index @f$ \mathbf{d}_{\max} @f$.
+         *
+         * @note
+         * @f[
+         *      d_{\max[k]} = 1 - d_{\min[k]}
+         * @f]
+         */
+        multi<int, N> dmax;
+
+        /**
+         * @brief Minimum parameter @f$ t_{\min} @f$.
+         */
+        float_type tmin;
+
+        /**
+         * @brief Maximum parameter @f$ t_{\max} @f$.
+         */
+        float_type tmax;
+    };
+
+    /**
+     * @brief Intersect ray?
+     */
+    __attribute__((always_inline))
+    bool intersect(const ray_info& r) const
+    {
+        // Intersect 0th slab.
+        float_type tmin = (arr_[r.dmin[0]][0] - r.o[0]) * r.dinv[0];
+        float_type tmax = (arr_[r.dmax[0]][0] - r.o[0]) * r.dinv[0];
+        tmax *= 1 + 2 * pr::numeric_limits<float_type>::echelon(3);
+
+        for (std::size_t k = 1; k < N; k++) {
+
+            // Intersect kth slab.
+            float_type tkmin = (arr_[r.dmin[k]][k] - r.o[k]) * r.dinv[k];
+            float_type tkmax = (arr_[r.dmax[k]][k] - r.o[k]) * r.dinv[k];
+            tkmax *= 1 + 2 * pr::numeric_limits<float_type>::echelon(3);
+            if (!(tmin <= tkmax &&
+                  tmax >= tkmin)) {
+                return false;
+            }
+
+            // Update.
+            tmin = pr::fmax(tmin, tkmin);
+            tmax = pr::fmin(tmax, tkmax);
+        }
+
+        // Clip.
+        return tmin <= r.tmax && 
+               tmax >= r.tmin;
     }
 
     /**@}*/
