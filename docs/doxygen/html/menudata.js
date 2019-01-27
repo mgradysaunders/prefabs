36c5a4841cc883852b04530cd90ad8b374c43c62
grady <mgradysaunders@gmail.com>
2019-01-27 15:39:19 -0500

    rerun doxygen

--- docs/doxygen/html/menudata.js
+++ docs/doxygen/html/menudata.js
@@ -22,7 +22,8 @@ var menudata={children:[
 {text:"p",url:"namespacemembers_p.html#index_p"},
 {text:"r",url:"namespacemembers_r.html#index_r"},
 {text:"s",url:"namespacemembers_s.html#index_s"},
-{text:"t",url:"namespacemembers_t.html#index_t"}]},
+{text:"t",url:"namespacemembers_t.html#index_t"},
+{text:"w",url:"namespacemembers_w.html#index_w"}]},
 {text:"Functions",url:"namespacemembers_func.html",children:[
 {text:"a",url:"namespacemembers_func.html#index_a"},
 {text:"c",url:"namespacemembers_func_c.html#index_c"},
@@ -40,7 +41,8 @@ var menudata={children:[
 {text:"p",url:"namespacemembers_func_p.html#index_p"},
 {text:"r",url:"namespacemembers_func_r.html#index_r"},
 {text:"s",url:"namespacemembers_func_s.html#index_s"},
-{text:"t",url:"namespacemembers_func_t.html#index_t"}]},
+{text:"t",url:"namespacemembers_func_t.html#index_t"},
+{text:"w",url:"namespacemembers_func_w.html#index_w"}]},
 {text:"Typedefs",url:"namespacemembers_type.html"},
 {text:"Enumerations",url:"namespacemembers_enum.html"}]}]},
 {text:"Classes",url:"annotated.html",children:[
