09778e2a534bd3aec036acb2a3cba8c5538d1c92
grady <mgradysaunders@gmail.com>
2019-02-17 16:39:18 -0500

    deriv

--- include/preform/interp.hpp
+++ include/preform/interp.hpp
@@ -52,19 +52,23 @@ namespace pr {
  * @param [in] t 
  * Factor.
  *
- * @param [in] x 
- * Value at @f$ t = 0 @f$.
+ * @param [in] p0
+ * Control point at @f$ t = 0 @f$.
  *
- * @param [in] y 
- * Value at @f$ t = 1 @f$.
+ * @param [in] p1
+ * Control point at @f$ t = 1 @f$.
  */
 template <
-    typename T, 
-    typename U
+    typename Tvalue, 
+    typename Tcontrol
     >
-constexpr auto lerp(T t, const U& x, const U& y)
+__attribute__((always_inline))
+constexpr auto lerp(
+            Tvalue t, 
+            const Tcontrol& p0, 
+            const Tcontrol& p1)
 {
-    return (1 - t) * x + t * y;
+    return (1 - t) * p0 + t * p1;
 }
 
 /**
@@ -73,30 +77,72 @@ constexpr auto lerp(T t, const U& x, const U& y)
  * @param [in] t 
  * Factor.
  *
- * @param [in] x 
- * Value at @f$ t = 0 @f$.
+ * @param [in] p0
+ * Control point at @f$ t = 0 @f$.
  *
- * @param [in] u 
+ * @param [in] m0
  * Slope at @f$ t = 0 @f$.
  *
- * @param [in] v 
+ * @param [in] m1
  * Slope at @f$ t = 1 @f$.
  *
- * @param [in] y 
- * Value at @f$ t = 1 @f$.
+ * @param [in] p1 
+ * Control point at @f$ t = 1 @f$.
  *
  * @see Wikipedia's article for [Cubic Hermite spline][1].
  * [1]: https://en.wikipedia.org/wiki/Cubic_Hermite_spline
  */
 template <
-    typename T,
-    typename U
+    typename Tvalue,
+    typename Tcontrol
     >
-constexpr auto hermite(T t, const U& x, const U& u, const U& v, const U& y)
+constexpr auto hermite(
+            Tvalue t, 
+            const Tcontrol& p0, 
+            const Tcontrol& m0, 
+            const Tcontrol& m1, 
+            const Tcontrol& p1)
 {
-    T s = 1 - t;
-    return (s * s * (1 + 2 * t)) * x + (s * s * t) * u +
-           (t * t * (3 - 2 * t)) * y - (t * t * s) * v;
+    Tvalue s = t - 1;
+    Tvalue h00 = s * s * (1 + 2 * t), h10 = s * s * t;
+    Tvalue h01 = t * t * (3 - 2 * t), h11 = t * t * s;
+    return (h00 * p0 + h10 * m0) +
+           (h01 * p1 + h11 * m1);
+}
+
+/**
+ * @brief Generate cubic Hermite interpolation deriviatve.
+ *
+ * @param [in] t 
+ * Factor.
+ *
+ * @param [in] p0
+ * Control point at @f$ t = 0 @f$.
+ *
+ * @param [in] m0
+ * Slope at @f$ t = 0 @f$.
+ *
+ * @param [in] m1
+ * Slope at @f$ t = 1 @f$.
+ *
+ * @param [in] p1 
+ * Control point at @f$ t = 1 @f$.
+ */
+template <
+    typename Tvalue,
+    typename Tcontrol
+    >
+constexpr auto hermite_deriv(
+            Tvalue t, 
+            const Tcontrol& p0, 
+            const Tcontrol& m0, 
+            const Tcontrol& m1, 
+            const Tcontrol& p1)
+{
+    Tvalue g00 = 6 * t * (t - 1);
+    Tvalue g10 = 3 * t * t - 4 * t + 1;
+    Tvalue g11 = 3 * t * t - 2 * t;
+    return g00 * (p0 - p1) + g10 * m0 + g11 * m1;
 }
 
 /**
@@ -105,28 +151,70 @@ constexpr auto hermite(T t, const U& x, const U& u, const U& v, const U& y)
  * @param [in] t 
  * Factor.
  *
- * @param [in] w 
- * Antecedent value at @f$ t = 0 @f$.
+ * @param [in] pprev
+ * Previous control point.
  *
- * @param [in] x 
- * Value at @f$ t = 0 @f$.
+ * @param [in] p0
+ * Control point at @f$ t = 0 @f$.
  *
- * @param [in] y 
- * Value at @f$ t = 1 @f$.
+ * @param [in] p1
+ * Control point at @f$ t = 1 @f$.
  *
- * @param [in] z
- * Subsequent value at @f$ t = 1 @f$.
+ * @param [in] pnext
+ * Next control point.
  *
  * @see Wikipedia's article for [Cubic Hermite spline][1].
  * [1]: https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Catmull–Rom_spline
  */
 template <
-    typename T,
-    typename U
+    typename Tvalue,
+    typename Tcontrol
     >
-constexpr auto catmull(T t, const U& w, const U& x, const U& y, const U& z)
+__attribute__((always_inline))
+constexpr auto catmull(
+            Tvalue t, 
+            const Tcontrol& pprev, 
+            const Tcontrol& p0, 
+            const Tcontrol& p1, 
+            const Tcontrol& pnext)
 {
-    return hermite(t, x, y - w, z - x, y);
+    return hermite(t, p0, p1 - pprev, pnext - p0, p1);
+}
+
+/**
+ * @brief Generic cubic Catmull-Rom interpolation derivative.
+ *
+ * @param [in] t 
+ * Factor.
+ *
+ * @param [in] pprev
+ * Previous control point.
+ *
+ * @param [in] p0
+ * Control point at @f$ t = 0 @f$.
+ *
+ * @param [in] p1
+ * Control point at @f$ t = 1 @f$.
+ *
+ * @param [in] pnext
+ * Next control point.
+ *
+ * @see Wikipedia's article for [Cubic Hermite spline][1].
+ * [1]: https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Catmull–Rom_spline
+ */
+template <
+    typename Tvalue,
+    typename Tcontrol
+    >
+__attribute__((always_inline))
+constexpr auto catmull_deriv(
+            Tvalue t, 
+            const Tcontrol& pprev, 
+            const Tcontrol& p0, 
+            const Tcontrol& p1, 
+            const Tcontrol& pnext)
+{
+    return hermite_deriv(t, p0, p1 - pprev, pnext - p0, p1);
 }
 
 /**@}*/
