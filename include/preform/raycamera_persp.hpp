603be980f03cd86026098b73126afe17c86a2581
grady <mgradysaunders@gmail.com>
2019-03-31 14:06:20 -0400

    revamp

--- include/preform/raycamera_persp.hpp
+++ include/preform/raycamera_persp.hpp
@@ -78,31 +78,6 @@ public:
 public:
 
     /**
-     * @brief Ray information.
-     */
-    struct ray_info
-    {
-    public:
-
-        /**
-         * @brief Origin @f$ \mathbf{o} @f$.
-         */
-        multi<float_type, 3> o = {};
-
-        /**
-         * @brief Direction @f$ \mathbf{d} @f$.
-         */
-        multi<float_type, 3> d = {};
-
-        /**
-         * @brief Pixel location @f$ \mathbf{p}_{\text{pix}} @f$.
-         */
-        multi<float_type, 2> ppix = {};
-    };
-
-public:
-
-    /**
      * @brief Default constructor.
      */
     raycamera_persp() = default;
@@ -110,8 +85,8 @@ public:
     /**
      * @brief Constructor.
      *
-     * @param[in] o
-     * Origin.
+     * @param[in] pc
+     * Position.
      *
      * @param[in] hatx
      * Basis direction.
@@ -122,11 +97,11 @@ public:
      * @param[in] hatz
      * Basis direction.
      *
-     * @param[in] w
-     * Screen width.
+     * @param[in] sx
+     * Screen size.
      *
-     * @param[in] h
-     * Screen height.
+     * @param[in] sy
+     * Screen size.
      *
      * @param[in] f
      * Y-FOV in radians.
@@ -139,34 +114,35 @@ public:
      *
      * @throw std::invalid_argument
      * Unless
-     * - `w > 0`,
-     * - `h > 0`, 
+     * - `sx > 0`,
+     * - `sy > 0`, 
      * - `f > 0 && f < pi`,
      * - `qmin[0] < qmax[0]`, and
      * - `qmin[1] < qmax[1]`.
      */
     raycamera_persp(
-        const multi<float_type, 3>& o,
+        const multi<float_type, 3>& pc,
         const multi<float_type, 3>& hatx,
         const multi<float_type, 3>& haty,
         const multi<float_type, 3>& hatz,
-        float_type w,
-        float_type h,
+        float_type sx,
+        float_type sy,
         float_type f,
         const multi<float_type, 2>& qmin = {0, 0},
         const multi<float_type, 2>& qmax = {1, 1}) :
-            o_(o),
+            pc_(pc),
             hatx_(hatx),
             haty_(haty),
             hatz_(hatz),
-            w_(w),
-            h_(h),
-            l_(h / (2 * pr::tan(f / 2))),
+            sx_(sx),
+            sy_(sy),
+            sz_(sy / (2 * pr::tan(f / 2))),
             qmin_(qmin),
-            qmax_(qmax)
+            qmax_(qmax),
+            a_(sx_ * sy_ / (sz_ * sz_) * (qmax_ - qmin_).prod())
     {
-        if (!(w > 0 &&
-              h > 0 &&
+        if (!(sx > 0 &&
+              sy > 0 &&
               f > 0 &&
               f < pr::numeric_constants<float_type>::M_pi() &&
               qmin[0] < qmax[0] &&
@@ -178,7 +154,7 @@ public:
     /**
      * @brief Look-at.
      *
-     * - @f$ \mathbf{o} \gets \mathbf{p}_0 @f$
+     * - @f$ \mathbf{p}_c \gets \mathbf{p}_0 @f$
      * - @f$ \hat{\mathbf{z}} \gets \normalize(\mathbf{p}_0 - \mathbf{p}_1) @f$
      * - @f$ \hat{\mathbf{x}} \gets \normalize(
      *           -\mathbf{y}_0\times \hat{\mathbf{z}}) @f$
@@ -194,11 +170,11 @@ public:
      * @param[in] y0
      * Up vector.
      *
-     * @param[in] w
-     * Screen width.
+     * @param[in] sx
+     * Screen size.
      *
-     * @param[in] h
-     * Screen height.
+     * @param[in] sy
+     * Screen size.
      *
      * @param[in] f
      * Y-FOV in radians.
@@ -218,22 +194,23 @@ public:
                 const multi<float_type, 3>& p0,
                 const multi<float_type, 3>& p1,
                 const multi<float_type, 3>& y0,
-                float_type w,
-                float_type h,
+                float_type sx,
+                float_type sy,
                 float_type f,
                 const multi<float_type, 2>& qmin = {0, 0},
                 const multi<float_type, 2>& qmax = {1, 1})
     {
-        multi<float_type, 3> o = p0;
+        multi<float_type, 3> pc = p0;
         multi<float_type, 3> hatz = normalize(p1 - p0);
         multi<float_type, 3> hatx = normalize(cross(-y0, hatz));
         multi<float_type, 3> haty = cross(hatz, hatx);
         return {
-            o, 
+            pc, 
             hatx, 
             haty, 
             hatz, 
-            w, h, 
+            sx,
+            sy, 
             f,
             qmin,
             qmax
@@ -241,35 +218,146 @@ public:
     }
 
     /**
-     * @brief Evaluate.
+     * @brief Importance weight.
+     *
+     * @f[
+     *      Z_e(\mathbf{p}, \omega) = 
+     *          \delta(\mathbf{p} - \mathbf{p}_c) \frac{1}{A\cos^3\theta}
+     * @f]
+     *
+     * @param[in] w
+     * Direction.
+     *
+     * @param[out] r
+     * Raster coordinate. _Optional_.
+     *
+     * @note
+     * As the position distribution is a delta on 
+     * @f$ \mathbf{p}_c @f$, the implementation here calculates 
+     * only the part of @f$ Z_e @f$ which depends on @f$ \omega @f$.
+     */
+    float_type ze(const multi<float_type, 3>& w,
+                        multi<float_type, 2>* r = nullptr) const
+    {
+        float_type cos_theta = dot(hatz_, w);
+        if (!(cos_theta > 0)) {
+            return 0;
+        }
+
+        // Normalized raster coordinate.
+        multi<float_type, 2> q = {
+            sz_ * dot(hatx_, w) / (sx_ * cos_theta) + float_type(0.5),
+            sz_ * dot(haty_, w) / (sy_ * cos_theta) + float_type(0.5)
+        };
+
+        // Clip.
+        if (!((q >= qmin_).all() &&
+              (q <= qmax_).all())) {
+            return 0;
+        }
+
+        // Raster coordinate.
+        if (r) {
+            *r = {
+                sx_ * q[0],
+                sy_ * q[1]
+            };
+        }
+
+        // Importance.
+        float_type cos2_theta = cos_theta * cos_theta;
+        float_type cos3_theta = cos_theta * cos2_theta;
+        return 1 / (a_ * cos3_theta);
+    }
+
+    /**
+     * @brief Importance weight sampling routine.
      *
      * @param[in] u
      * Sample in @f$ [0, 1)^2 @f$.
+     *
+     * @param[out] p
+     * Position.
+     *
+     * @param[out] w
+     * Direction.
+     *
+     * @param[out] r
+     * Raster coordinate. _Optional_.
      */
-    ray_info operator()(const multi<float_type, 2>& u) const
+    float_type ze_sample(
+            const multi<float_type, 2>& u,
+            multi<float_type, 3>& p,
+            multi<float_type, 3>& w,
+            multi<float_type, 2>* r = nullptr) const
     {
-        ray_info ray;
-        ray.o = o_;
+        // Position.
+        p = pc_;
+
         multi<float_type, 2> q = (1 - u) * qmin_ + u * qmax_;
-        multi<float_type, 3> dx = w_ * (q[0] - float_type(0.5)) * hatx_;
-        multi<float_type, 3> dy = h_ * (q[1] - float_type(0.5)) * haty_;
-        multi<float_type, 3> dz = l_ * hatz_;
-        ray.d = fastnormalize(dx + dy + dz);
-        ray.ppix = {
-            w_ * q[0],
-            h_ * q[1]
+        multi<float_type, 3> v = {
+            sx_ * (q[0] - float_type(0.5)),
+            sy_ * (q[1] - float_type(0.5)),
+            sz_
         };
-        return ray;
+        // Direction.
+        w = fastnormalize(
+                hatx_ * v[0] + 
+                haty_ * v[1] +
+                hatz_ * v[2]);
+
+        // Raster coordinate.
+        if (r) {
+            *r = {
+                sx_ * q[0],
+                sy_ * q[1]
+            };
+        }
+
+        // Importance.
+        float_type cos_theta = v[2] / fastlength(v);
+        float_type cos2_theta = cos_theta * cos_theta;
+        float_type cos3_theta = cos_theta * cos2_theta;
+        return 1 / (a_ * cos3_theta);
     }
 
-    // TODO importance functions
+    /**
+     * @brief Importance weight solid angle sampling routine.
+     *
+     * With respect to a reference point @f$ \mathbf{p}_{\text{ref}} @f$,
+     * sample a direction @f$ \omega_i @f$ within the solid-angle the camera
+     * subtends, and calculate the corresponding importance @f$ Z_i @f$.
+     * @f[
+     *      Z_i(\omega_i; \mathbf{p}_{\text{ref}}) = 
+     *      \frac{1}{d^2} Z_e(\mathbf{p}_{\text{hit}}, -\omega_i)
+     * @f]
+     *
+     * @param[in] pref
+     * Reference point.
+     *
+     * @param[out] wi
+     * Incident direction.
+     */
+    float_type zi_sample(
+            const multi<float_type, 3>& pref,
+                  multi<float_type, 3>& wi) const
+    {
+        wi = pc_ - pref;
+        float_type d2 = dot(wi, wi);
+        if (!(d2 > 0)) {
+            return 0;
+        }
+
+        // Inverse square falloff.
+        return ze(-(wi /= pr::sqrt(d2))) / d2;
+    }
 
 private:
 
     /**
-     * @brief Origin @f$ \mathbf{o} @f$.
+     * @brief Position @f$ \mathbf{p}_c @f$.
      */
-    multi<float_type, 3> o_;
+    multi<float_type, 3> pc_;
 
     /**
      * @brief Basis direction @f$ \hat{\mathbf{x}} @f$.
@@ -287,19 +375,25 @@ private:
     multi<float_type, 3> hatz_;
 
     /**
-     * @brief Screen width @f$ w @f$.
+     * @brief Screen size @f$ s_x @f$.
      */
-    float_type w_ = 0;
+    float_type sx_ = 0;
 
     /**
-     * @brief Screen height @f$ h @f$.
+     * @brief Screen size @f$ s_y @f$.
      */
-    float_type h_ = 0;
+    float_type sy_ = 0;
 
     /**
-     * @brief Pinhole distance @f$ l @f$.
+     * @brief Screen distance @f$ s_z @f$.
+     *
+     * @f[
+     *      s_z = 
+     *      \frac{s_y}{2} 
+     *      \cot\left[\frac{f}{2}\right]
+     * @f]
      */
-    float_type l_ = 0;
+    float_type sz_ = 0;
 
     /**
      * @brief Sub-region bounding point @f$ \mathbf{q}_{\min} @f$.
@@ -310,6 +404,22 @@ private:
      * @brief Sub-region bounding point @f$ \mathbf{q}_{\max} @f$.
      */
     multi<float_type, 2> qmax_ = {1, 1};
+
+    /**
+     * @brief Normalization @f$ A @f$.
+     *
+     * @f[
+     * \begin{aligned}
+     *      A &= \alpha s_x s_y \frac{1}{s_z^2}
+     * \\     &= \alpha \frac{s_x}{s_y} \cdot 4\tan^2\left[\frac{f}{2}\right]
+     * \end{aligned}
+     * @f]
+     * where @f$ \alpha = 
+     * ({q_{\max}}_x - {q_{\min}}_x) 
+     * ({q_{\max}}_y - {q_{\min}}_y) @f$
+     * 
+     */
+    float_type a_ = 0;
 };
 
 /**@}*/
