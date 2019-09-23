977cfebbc9a77f868570b0b6cb7713284c976498
grady <mgradysaunders@gmail.com>
2019-09-23 13:52:04 -0400

    max/min to promote fp instructions

--- include/preform/float_interval.hpp
+++ include/preform/float_interval.hpp
@@ -230,9 +230,9 @@ public:
         T dx0 = x_ - x0_;
         T dx1 = x1_ - x_;
         std::fesetround(mode);
-        return pr::fmax(dx0, dx1);
+        return pr::max(dx0, dx1);
 #else
-        return pr::finc(pr::fmax(x_ - x0_, x1_ - x_));
+        return pr::finc(pr::max(x_ - x0_, x1_ - x_));
 #endif // #if PREFORM_USE_FENV
     }
 
@@ -632,8 +632,8 @@ inline float_interval<T> operator*(
     };
     return {
         b0.value() * b1.value(),
-        pr::fdec(pr::fmin(pr::fmin(tmp[0], tmp[1]), pr::fmin(tmp[2], tmp[3]))),
-        pr::finc(pr::fmax(pr::fmax(tmp[0], tmp[1]), pr::fmax(tmp[2], tmp[3])))
+        pr::fdec(pr::min(pr::min(tmp[0], tmp[1]), pr::min(tmp[2], tmp[3]))),
+        pr::finc(pr::max(pr::max(tmp[0], tmp[1]), pr::max(tmp[2], tmp[3])))
     };
 }
 
@@ -674,8 +674,8 @@ inline float_interval<T> operator/(
     };
     return {
         b0.value() / b1.value(),
-        pr::fdec(pr::fmin(pr::fmin(tmp[0], tmp[1]), pr::fmin(tmp[2], tmp[3]))),
-        pr::finc(pr::fmax(pr::fmax(tmp[0], tmp[1]), pr::fmax(tmp[2], tmp[3])))
+        pr::fdec(pr::min(pr::min(tmp[0], tmp[1]), pr::min(tmp[2], tmp[3]))),
+        pr::finc(pr::max(pr::max(tmp[0], tmp[1]), pr::max(tmp[2], tmp[3])))
     };
 }
 
