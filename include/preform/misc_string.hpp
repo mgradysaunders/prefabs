f2d1df8241e9b8ee35cffcfed1b4238cabe8e7ba
grady <mgradysaunders@gmail.com>
2020-01-29 16:36:29 -0500

    case insensitive strings

--- include/preform/misc_string.hpp
+++ include/preform/misc_string.hpp
@@ -38,6 +38,12 @@
 // for std::snprintf
 #include <cstdio>
 
+// for std::toupper, std::tolower
+#include <cctype>
+
+// for std::towupper, std::towlower
+#include <cwctype>
+
 // for std::string
 #include <string>
 
@@ -110,6 +116,101 @@ inline std::string str_typename()
     return std::string(typeid(T).name());
 #endif // #if __GNUC__
 }
+
+#if !DOXYGEN
+
+template <typename Tchar>
+struct ci_to_upper;
+
+template <>
+struct ci_to_upper<char> {
+    __attribute__((always_inline))
+    static auto to_upper(char c)
+    {
+        return std::toupper(static_cast<unsigned char>(c));
+    }
+};
+
+template <>
+struct ci_to_upper<wchar_t> {
+    __attribute__((always_inline))
+    static auto to_upper(wchar_t c)
+    {
+        return std::towupper(static_cast<std::wint_t>(c));
+    }
+};
+
+#endif // #if !DOXYGEN
+
+/**
+ * @brief Case-insensitive character traits.
+ */
+template <typename Tchar>
+struct ci_char_traits : public std::char_traits<Tchar>
+{
+public:
+
+    // Sanity check.
+    static_assert(
+        std::is_same<Tchar, char>::value ||
+        std::is_same<Tchar, wchar_t>::value,
+        "Tchar must be char or wchar_t");
+
+    /**
+     * @brief Compare equal.
+     */
+    static bool eq(Tchar c1, Tchar c2)
+    {
+        return ci_to_upper<Tchar>::to_upper(c1) == 
+               ci_to_upper<Tchar>::to_upper(c2);
+    }
+
+    /**
+     * @brief Compare less-than.
+     */
+    static bool lt(Tchar c1, Tchar c2)
+    {
+        return ci_to_upper<Tchar>::to_upper(c1) <
+               ci_to_upper<Tchar>::to_upper(c2);
+    }
+
+    /**
+     * @brief Compare lexicographical.
+     */
+    static int compare(const Tchar* s1, const Tchar* s2, std::size_t n)
+    {
+        for (; n-- != 0; ++s1, ++s2) {
+            auto c1 = ci_to_upper<Tchar>::to_upper(*s1);
+            auto c2 = ci_to_upper<Tchar>::to_upper(*s2);
+            if (c1 < c2) { return -1; }
+            if (c2 < c1) { return +1; }
+        }
+        return 0;
+    }
+
+    /**
+     * @brief Find character in string.
+     */
+    static const Tchar* find(const Tchar* s, int n, Tchar c)
+    {
+        for (; n-- > 0; ++s) {
+            if (eq(*s, c)) {
+                return s;
+            }
+        }
+        return nullptr;
+    }
+};
+
+/**
+ * @brief Case-insensitive string.
+ */
+using ci_string = std::basic_string<char, ci_char_traits<char>>;
+
+/**
+ * @brief Case-insensitive wide string.
+ */
+using ci_wstring = std::basic_string<wchar_t, ci_char_traits<wchar_t>>;
 
 /**@}*/
 
